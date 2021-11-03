1527413d110a062f5769e18791331e9b7b5b3f27
Ilan Grynszpan <ilan.grynszpan@gmail.com>
2021-11-02 21:47:10 -0300

    Added responsive navbar and page elements, still lacking on layout and horizontal list group responsiveness

--- react/v1/cites/src/App.js
+++ react/v1/cites/src/App.js
@@ -4,9 +4,11 @@ import CategoryList from './components/category_list.js';
 import TitleBar from './components/title_bar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import {Button} from 'react-bootstrap';
+
 function App() {
 
-  let nb_option = ["home", "features","pricing"]
+  let nb_option = ["Entrar", "Crie sua conta","Cadastre seu serviço"]
 
   return (
     <div className="App">
@@ -14,7 +16,15 @@ function App() {
         nb_title="cites"
         option={nb_option}
       />
+      <p>Escolha abaixo o serviço que precisa e encontraremos profissionais para lhe atender:</p>
       <CategoryList/>
+      <p>Conhece um bom profissional?</p>
+      <p>Alguém de confiança que pode ajudar muitas pessoas?</p>
+      <Button variant="primary">Clique aqui para nos recomendar!</Button>
+      <footer>
+      © 2020 Copyright:
+            Icons made by <a href="https://www.flaticon.com/authors/photo3idea-studio" title="photo3idea_studio">photo3idea_studio</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+      </footer>
     </div>
   );
 }
--- react/v1/cites/src/components/title_bar.js
+++ react/v1/cites/src/components/title_bar.js
@@ -6,14 +6,17 @@ class TitleBar extends React.Component {
   render() {
       return (
          <div>
-              <Navbar bg="light" variant="light" expand="lg" sticky="top">
-                <Container>
-                <Navbar.Brand href="#home">{this.props.nb_title}</Navbar.Brand>
+              <Navbar bg="light" variant="light" expand="sm" sticky="top">
+                <Container fluid>
+                <Navbar.Brand href="#home"  style={{color:"red"}}>{this.props.nb_title}</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
                   <Nav.Link href="#home">{this.props.option[0]}</Nav.Link>
                   <Nav.Link href="#features">{this.props.option[1]}</Nav.Link>
                   <Nav.Link href="#pricing">{this.props.option[2]}</Nav.Link>
                 </Nav>
+                </Navbar.Collapse>
                 </Container>
               </Navbar>
          </div>
